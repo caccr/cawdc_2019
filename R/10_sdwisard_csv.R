500a0def0bf517f71f8ec7ef6c3396745db26c93
Rich Pauloo <richpauloo@gmail.com>
2020-08-18 09:25:18 -0700

    gz file conversion of all csvs

--- R/10_sdwisard_csv.R
+++ R/10_sdwisard_csv.R
@@ -11,20 +11,37 @@ if(!dir.exists(dir)) { dir.create(dir) }
 
 # only a 7% reduction in observations if non community water
 # systems are excluded, so keep them
-d2 <- d %>% split(.$PWSID) 
+d2 <- d %>% split(.$PWSID)
 
 for(i in 1:length(d2)){
   write_csv( d2[[i]], paste0(dir, d2[[i]]$PWSID[1], ".csv") )
 }
 
-names(d2) %>% 
+names(d2) %>%
   walk(
     ~ write_csv(
-      d2[[.]], 
+      d2[[.]],
       paste0(dir, d2[[.]]$PWSID[1], ".csv")
     )
   )
 
+# ------------------------------------------------------------------------
+# convert all csv to gz for S3
+# ------------------------------------------------------------------------
+
+f  <- list.files("~/Desktop/s3")
+ff <- list.files("~/Desktop/s3", full.names = TRUE)
+l  <- vector("list", length=length(f))
+for(i in seq_along(l)) {
+  l[[i]] <- data.table::fread(ff[i])
+}
+
+pth <- paste0("~/Desktop/gz/", f, ".gz")
+for(i in seq_along(f)){
+  gz <- gzfile(pth[i], "w")
+  readr::write_csv(l[[i]], gz)
+  close(gz)
+}
 
 
 
